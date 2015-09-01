bc405b06c2985cc24041329a8890bd8a1242116e
Dean <deanhume@gmail.com>
2015-09-01 17:37:43 +0100

    Updated worker to intercept jpeg requests

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,41 @@
 
 // Register the service worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+	navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
     // Registration was successful
     console.log('ServiceWorker registration successful with scope: ',    registration.scope);
-  }).catch(function(err) {
+}).catch(function(err) {
     // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
-  });
+    	console.log('ServiceWorker registration failed: ', err);
+    });
 }
 
-// self.onfetch = function(event) {
-//   var req = event.request.clone();
-//   console.log("SW received request for: " + req.url)
-//   for (entry of req.headers.entries()) { 
-//     console.log("\t" + entry[0] +": " + entry[1]) 
-//   }
-// }
-
-
-// self.addEventListener('onfetch', function(event) { 
-// 		debugger;
-//  console.log("fetch registered");
-
-// var req = event.request.clone();
-// for (entry of req.headers.entries()) { 
-//     console.log("\t" + entry[0] +": " + entry[1]) 
-//   }
-// });
-
+// Listen to fetch events
 self.addEventListener('fetch', function(event) {
-  console.log("Caught a fetch!");
-  
-  var req = event.request.clone();
-  var result = '';
-  for (entry of req.headers.entries()) { 
-    result += "\t" + entry[0] +": " + entry[1];
-  }
 
-  event.respondWith(new Response(result));
+	// Clone the request
+	var req = event.request.clone();
+
+  	// Check if the image is a jpeg
+  	if (/\.jpg$/.test(event.request.url)) {
+
+		// Check the headers
+		for (entry of req.headers.entries()) { 
+			// Check the accept header
+			if (entry[0] == 'accept')
+			{
+				if (entry[1].includes('webp'))
+				{
+					// change the return path
+					var returnUrl = req.url.substr(0, req.url.lastIndexOf(".")) + ".webp";
+
+					event.respondWith(
+						fetch(returnUrl, {
+							mode: 'no-cors'
+						})
+					);
+				}
+			}
+		}
+	}
 });
