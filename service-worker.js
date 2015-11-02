36e9d19a5d5aebc8fe58aabb93e2b93f50e62423
Dean <deanhume@gmail.com>
2015-11-02 10:33:38 +0000

    Added basic code example to handle higher DPR

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,8 @@
-
-// Register the service worker
+ // Register the service worker
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
     // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
 }).catch(function(err) {
     // registration failed :(
     	console.log('ServiceWorker registration failed: ', err);
@@ -17,25 +16,48 @@ self.addEventListener('fetch', function(event) {
 	var req = event.request.clone();
 
   	// Check if the image is a jpeg
-  	if (/\.jpg$/.test(event.request.url)) {
+  	if (/\.jpg$|.png$/.test(event.request.url)) {
+
+  		var dpr = 1;
+  		var supportsWebp = false;
 
 		// Check the headers
 		for (entry of req.headers.entries()) { 
+			
 			// Check the accept header
-			if (entry[0] == 'accept')
+			if (entry[0].toLowerCase() == 'accept' && entry[1].toLowerCase().includes('webp'))
 			{
-				if (entry[1].includes('webp'))
-				{
-					// change the return path
-					var returnUrl = req.url.substr(0, req.url.lastIndexOf(".")) + ".webp";
-
-					event.respondWith(
-						fetch(returnUrl, {
-							mode: 'no-cors'
-						})
-					);
-				}
+				supportsWebp = true;
 			}
+
+			// Check the client hints header
+			if (entry[0].toLowerCase() == 'dpr')
+			{
+				dpr = 2;
+			}
+		}
+
+		if (supportsWebp)
+		{
+			var returnUrl;
+
+			// If we have a high DPR then change path
+			if (dpr > 1)
+			{
+				returnUrl = req.url.substr(0, req.url.lastIndexOf(".")) + "-" + dpr + "x.webp";
+			}
+			else{
+				// Just return the normal webp url
+				returnUrl = req.url.substr(0, req.url.lastIndexOf(".")) + ".webp";
+			}
+
+			console.log(returnUrl);
+
+			event.respondWith(
+				fetch(returnUrl, {
+					mode: 'no-cors'
+				})
+			);
 		}
 	}
 });
