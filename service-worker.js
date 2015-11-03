dc2127601e442f7f613c17391fbd007a26df4f02
Dean <deanhume@gmail.com>
2015-11-03 11:04:07 +0000

    Updated with latest changes

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,4 @@
-
-// Register the service worker
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-    // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
-}).catch(function(err) {
-    // registration failed :(
-    	console.log('ServiceWorker registration failed: ', err);
-    });
-}
+"use strict";
 
 // Listen to fetch events
 self.addEventListener('fetch', function(event) {
@@ -17,25 +7,26 @@ self.addEventListener('fetch', function(event) {
 	var req = event.request.clone();
 
   	// Check if the image is a jpeg
-  	if (/\.jpg$/.test(event.request.url)) {
+  	if (/\.jpg$|.png$/.test(event.request.url)) {
 
-		// Check the headers
-		for (entry of req.headers.entries()) { 
-			// Check the accept header
-			if (entry[0] == 'accept')
-			{
-				if (entry[1].includes('webp'))
-				{
-					// change the return path
-					var returnUrl = req.url.substr(0, req.url.lastIndexOf(".")) + ".webp";
+  		// Get all of the headers
+  		let headers = Array.from(req.headers.entries());
 
-					event.respondWith(
-						fetch(returnUrl, {
-							mode: 'no-cors'
-						})
-					);
-				}
-			}
-		}
+  		// Inspect the accept header for WebP support
+  		var acceptHeader = headers.find(item => item[0] == 'accept');
+  		var supportsWebp = acceptHeader[1].includes('webp');
+
+  		// If we support WebP
+  		if (supportsWebp)
+  		{
+        // Build the return URL
+  			var returnUrl = req.url.substr(0, req.url.lastIndexOf(".")) + ".webp";
+
+          event.respondWith(
+          fetch(returnUrl, {
+            mode: 'no-cors'
+          })
+        );
+  	}
 	}
 });
